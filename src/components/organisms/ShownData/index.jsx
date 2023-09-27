e31b838cbb6973339f5afbc44b5f431286a79e04
ryanderon <ryansinaga59@gmail.com>
2023-09-27 23:23:18 +0700

    remove unused import

--- src/components/organisms/ShownData/index.jsx
+++ src/components/organisms/ShownData/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import Barcode from "react-barcode";
 import Typo from "../../atoms/Typo";
 import FlexBox from "../../atoms/FlexBox";
