0da18ff20b90b4214606f847cfe49e12f931e345
ryanderon <ryansinaga59@gmail.com>
2023-10-09 21:02:31 +0700

    Revert "move ddl data to helpers"
    This reverts commit c7d961f4c37ff63a1fbfef3613cbcef15cddac02.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,36 @@ import FlexBox from "./components/atoms/FlexBox";
 import LoadingSpinner from "./components/molecules/LoadingSpinner";
 import ShownData from "./components/organisms/ShownData";
 import Typo from "./components/atoms/Typo";
-import { rowDDL } from "./helpers";
+
+const rowDDL = [
+  "ALL ROW",
+  "ROW 01",
+  "ROW 02",
+  "ROW 03",
+  "ROW 04",
+  "ROW 05",
+  "ROW 06",
+  "ROW 07",
+  "ROW 08",
+  "ROW 09",
+  "ROW 10",
+  "ROW 11",
+  "ROW 12",
+  "ROW 13",
+  "ROW 14",
+  "ROW 15",
+  "ROW 16",
+  "ROW 17",
+  "ROW 18",
+  "ROW 19",
+  "ROW 20",
+  "ROW 31",
+  "ROW 32",
+  "ROW 33",
+  "ROW 34",
+  "ROW 35",
+  "BM",
+];
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
